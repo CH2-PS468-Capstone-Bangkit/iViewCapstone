cfef9cf55a235e7adefb9bb56239d78862aa4044
Intan Charolina <intancharolina079@student.uns.ac.id>
2023-12-17 20:59:50 +0700

    feat(dashboard): Making Scan Article

--- app/src/main/java/com/project/iviewcapstone/ui/screen/ScreenActivity.kt
+++ app/src/main/java/com/project/iviewcapstone/ui/screen/ScreenActivity.kt
@@ -1,12 +1,25 @@
 package com.project.iviewcapstone.ui.screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.iviewcapstone.R
+import com.project.iviewcapstone.databinding.ActivityScreenBinding
+import com.project.iviewcapstone.signin.SignInActivity
+import com.project.iviewcapstone.signup.SignUpActivity
 
 class ScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_screen)
+
+        binding = ActivityScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnScreen1.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
